c0396ee165700fb44d9ca89c2c73d371f119fa7f
Bobby H <rhoffman103@gmail.com>
2018-08-17 11:04:19 -0400

    initial commit

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # liri-node-app
+
+LIRI will be a command line node app that takes in parameters and will search Spotify for songs, Bands in Town for concerts, and OMDB for movies.
---
+++ liri.js
@@ -0,0 +1,7 @@
+//  read and set any environment variables with the dotenv package
+require("dotenv").config();
+
+//  Need code to import keys.js and store it in a variable
+
+//   Access keys info
+var spotify = new spotify(keys.spotify);
